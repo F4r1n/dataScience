e7da17074b944ea140ed874a48c2ee1cc11b3f8b
Jonas Liebermann <jonas.liebermann@senseaition.com>
2019-11-01 13:56:51 +0100

    first scripts

---
+++ webscraper/getlist.py
@@ -0,0 +1,19 @@
+import requests
+import bs4
+
+#request site with list of all scripts
+res = requests.get('https://www.imsdb.com/all%20scripts/')
+#if request was successfull
+if res.status_code == requests.codes.ok:
+    soup = bs4.BeautifulSoup(res.text, features="lxml")
+    #search for the table
+    td = soup.select('td')
+    #Each movie has a link
+    titles = td[86].find_all('a')
+    #open a file for the list
+    with open("list.txt", "w") as file:
+        #iterate over all the movies
+        for movie in titles:
+            movieName = movie.getText()
+            #write to file
+            file.write("%s\n" % movieName)
---
+++ webscraper/scrape.py
@@ -0,0 +1,43 @@
+import bs4
+import requests
+import os
+
+#scriptToFile takes the fileName and html and writes it to a file
+def scriptToFile(name, text):
+    #Open new file in data directory
+    with open(os.getcwd() + "/data/%s.txt" % name, "w") as file:
+        soup = bs4.BeautifulSoup(text, features="lxml")
+        #search for pre element which holds the script
+        pre = soup.select('pre')
+        #some scripts start with the string that is being replaced thus we remove it
+        filterPre = pre[0].getText().replace("""
+
+
+<b><!--
+</b>if (window!= top)
+top.location.href=location.href
+<b>// -->
+</b>""" , "")
+        #write filtered element to file
+        file.write(filterPre)
+
+#open file to store errors in to review them manually
+with open("errors.txt", 'a') as file:
+    #open file containing the list of movies
+    with open("list.txt", 'r') as list:
+        #iterate file line by line
+        for movie in list:
+            #replace blank spaces with - to use in the url
+            movieString = movie.replace(" ", "-")
+            #replace newlines for fromatting
+            movieString = movieString.replace("\n", "")
+            #replace : because its not used by the site in the url
+            movieString = movieString.replace(":", "")
+            #request the page with the script
+            res = requests.get('https://www.imsdb.com/scripts/%s.html' % movieString)
+            #if okay call function that saves the script
+            if res.status_code == requests.codes.ok:
+                scriptToFile(movieString, res.text)
+            #on error write movie title to error file
+            else:
+                file.write(movie)
