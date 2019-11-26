42bee530f91041f46cd6e05399099b528f00138c
Jonas Liebermann <jonas.liebermann@senseaition.com>
2019-11-26 18:16:45 +0100

    sentiment and csv cleanup

--- processing/processing_new.py
+++ processing/processing_new.py
@@ -1,16 +1,16 @@
-#Data Analysis
+# Data Analysis
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#Data Preprocessing and Feature Engineering
+# Data Preprocessing and Feature Engineering
 from textblob import TextBlob
 import re
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
-#Model Selection and Validation
+# Model Selection and Validation
 # from sklearn.naive_bayes import MultinomialNB
 # from sklearn.model_selection import train_test_split
 # from sklearn.pipeline import Pipeline
@@ -19,6 +19,7 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 import re
 import nltk
 import os
+import json
 import wordcloud
 import matplotlib.pylab as plt
 from collections import Counter
@@ -27,6 +28,7 @@ nltk.download('stopwords')
 # Get a list of stopwords from nltk
 stopwords = nltk.corpus.stopwords.words("english")
 
+
 def get_clean_words(words):
     def _isnum(w):
         try:
@@ -34,10 +36,10 @@ def get_clean_words(words):
             return True
         except ValueError:
             return False
-        
+
     # Set words to lowercase and remove them if they are stop words
     words = [w.lower() for w in words if w.lower() not in stopwords]
-    
+
     # Remove punctuation
     words = [w.replace('(', '') for w in words]
     words = [w.replace(')', '') for w in words]
@@ -49,26 +51,63 @@ def get_clean_words(words):
 
     # Remove numbers
     words = [w for w in words if not _isnum(w)]
-    
+
     # Only keep words with more than one character
     words = [w for w in words if len(w) > 1]
-    
+
     return words
-    
-    
+
+
+def chunkIt(seq, num):
+    avg = len(seq) / float(num)
+    out = []
+    last = 0.0
+
+    while last < len(seq):
+        out.append(seq[int(last):int(last + avg)])
+        last += avg
+
+    return out
+
+
 def word_count(text):
     return Counter(text.split())
 
+
 movies = []
 file_words = []
-for filename in os.listdir('./data/'):
-    movie = filename.replace('.txt', '')
-    movie = movie.replace('-', ' ')
-    movies.append(movie)
-    with open('./data/' + filename, "r") as file:
-        data = file.read().replace('\n', '')
-        words = data.split()
-        file_words.append(words)
+
+with open("features.json", "r+") as jsonFile:
+    # data = json.load(jsonFile)
+    with open("list.txt", 'r') as list:
+        for movie in list:
+            movie = movie.replace("\n", "")
+            # replace blank spaces with - to use in the url
+            movieString = movie.replace(" ", "-")
+            # replace : because its not used by the site in the url
+            movieString = movieString.replace(":", "")
+            try:
+                with open('./data/' + movieString + ".txt", "r") as file:
+                    script = file.read().replace('\n', '')
+                    words = script.split()
+                    file_words.append(words)
+                    movies.append(movie)
+                    # Split movie script into five even parts
+                    # movieParts = chunkIt(words, 5)
+                    # data[movie]["parts"] = {}
+                    # for i, moviePart in enumerate(movieParts):
+                    #     textblob = TextBlob(' '.join(moviePart))
+                    #     partSentiment = textblob.sentiment
+                    #     data[movie]["parts"][str(i + 1)] = {}
+                    #     data[movie]["parts"][str(
+                    #         i + 1)]["sentiment"] = partSentiment
+                    # scriptBlob = TextBlob(script)
+                    # data[movie]["overAllSentiment"] = scriptBlob.sentiment
+            except:
+                print("Movie not found")
+    # jsonFile.seek(0)
+    # json.dump(data, jsonFile, indent=4)
+    # jsonFile.truncate()
 
 corpus = {}
 word_counts = []
@@ -84,8 +123,8 @@ with open('movies.csv', 'w') as f:
     for key in corpus.keys():
         my_dict = word_count(corpus[key])
         for k in my_dict.keys():
-            if my_dict[k] > 3:
-                f.write("%s,%s,%s\n"%(key,k,my_dict[k]))
+            if my_dict(k) > 4:
+                f.write("%s,%s,%s\n" % (key, k, my_dict[k]))
 
 
 # i = 0
